ed9033a363c107a9817bcf2546e2d407b753a571
ALANKRIT AGARWAL <itews2216@glbitm.ac.in>
2025-01-01 23:12:56 +0530

    DB connection established

--- Backend/db.js
+++ Backend/db.js
@@ -2,7 +2,7 @@ import mongoose, { mongo } from "mongoose";
 
 const connectMongoDB = async () => {
 	try {
-        const mongo_uri = "mongodb+srv://ninjacoder811:sG7hRS9fe2rNxZzp@cluster0.oy9be.mongodb.net/AlmametaDB?retryWrites=true&w=majority&appName=Cluster0"
+        const mongo_uri = "mongodb+srv://alan_agarwal_P2:COTclWAgA1igWjBp@cluster0.4tqfu.mongodb.net/alma-mate"
 		const conn = await mongoose.connect(mongo_uri);
 		console.log(`MongoDB connected: ${conn.connection.host}`);
 	} catch (error) {
