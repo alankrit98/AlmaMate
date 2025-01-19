2996ecacdaf82ebc44f8a63a1bcef0e86764552f
ALANKRIT AGARWAL <itews2216@glbitm.ac.in>
2025-01-19 16:22:17 +0530

    Connection Re-established

--- Backend/server.js
+++ Backend/server.js
@@ -21,7 +21,7 @@ app.get("/user" , (req,res) => {
     res.send({data});
   });
 });
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(4000, () => {
+  console.log('Server is running on port 4000');
   db();
 });
